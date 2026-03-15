698c25cf663ed6793fd7c7d24ec2904ef6e1d579
SrRomer0 <diego.romero.vera.11@gmail.com>
2026-03-15 12:15:05 +0100

    Traducciones i18 añadidas

--- src/main/java/es/refugio/animales/vista/infraestructure/web/enums/FragmentoContenido.java
+++ src/main/java/es/refugio/animales/vista/infraestructure/web/enums/FragmentoContenido.java
@@ -3,17 +3,17 @@ package es.refugio.animales.vista.infraestructure.web.enums;
 public enum FragmentoContenido {
 
     Persona_LIST("fragments/content/personas-lista"),
-    Persona_FORM("fragments/content/Persona-formulario"),
-    Persona_CREATED("fragments/content/Persona-creado"),
+    Persona_FORM("fragments/content/persona-formulario"),
+    Persona_CREATED("fragments/content/persona-creada"),
 
     Voluntario_LIST("fragments/content/voluntarios-lista"),
-    Voluntario_FORM("fragments/content/Voluntario-formulario"),
+    Voluntario_FORM("fragments/content/voluntario-formulario"),
 
     Animal_LIST("fragments/content/animales-lista"),
-    Animal_FORM("fragments/content/Animal-formulario"),
+    Animal_FORM("fragments/content/animal-formulario"),
 
     Adopcion_LIST("fragments/content/adopciones-lista"),
-    Adopcion_FORM("fragments/content/Adopcion-formulario"),
+    Adopcion_FORM("fragments/content/adopcion-formulario"),
 
     HOME_VIEW("fragments/content/index");
 
